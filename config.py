2df48bf810297a54a760d390d4b7d5ac781f5a98
firstadhi <44322688+firstadhi@users.noreply.github.com>
2025-07-02 16:53:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
-TELEGRAM_TOKEN = '7595723761:AAE51oW9XFrvCpqcq83yx-q9XkCNKUKvKJA'
-CHAT_ID = '846561600'
-DATABASE = 'news.db'
-NEWS_KEYWORD = 'BPJS Ketenagakerjaan'
-NEWS_SITE = 'https://news.google.com/search?q=' + NEWS_KEYWORD.replace(' ', '%20')
-NEWSAPI_KEY = "75b802ccc803409b9d2ff730df11c733"
-# config.py
-GOOGLE_CSE_API_KEY = "AIzaSyDaLwdKvUrbHXu7zzCr7XknRPg6EzmyKiA"
-GOOGLE_CSE_CX = "40c862fcde8f54d7d"
+import os
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+DATABASE = os.getenv('DATABASE', 'news.db')
+NEWS_KEYWORD = os.getenv('NEWS_KEYWORD', 'BPJS Ketenagakerjaan')
+NEWS_SITE = f"https://news.google.com/search?q={NEWS_KEYWORD.replace(' ', '%20')}"
+NEWSAPI_KEY = os.getenv('NEWSAPI_KEY')
+GOOGLE_CSE_API_KEY = os.getenv('GOOGLE_CSE_API_KEY')
+GOOGLE_CSE_CX = os.getenv('GOOGLE_CSE_CX')
