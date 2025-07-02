7687690e327697bf2790eba764722a9b2dfdbd68
firstadhi <44322688+firstadhi@users.noreply.github.com>
2025-07-02 16:29:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ DATABASE = 'news.db'
 NEWS_KEYWORD = 'BPJS Ketenagakerjaan'
 NEWS_SITE = 'https://news.google.com/search?q=' + NEWS_KEYWORD.replace(' ', '%20')
 NEWSAPI_KEY = "75b802ccc803409b9d2ff730df11c733"
+# config.py
+GOOGLE_CSE_API_KEY = "AIzaSyDaLwdKvUrbHXu7zzCr7XknRPg6EzmyKiA"
+GOOGLE_CSE_CX = "40c862fcde8f54d7d"
