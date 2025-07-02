37d49fb8b2de028d329f267536c7634012dc02b1
firstadhi <44322688+firstadhi@users.noreply.github.com>
2025-07-03 00:49:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-# Muat .env jika dijalankan secara lokal
-load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-DATABASE = os.getenv("DATABASE", "news.json")
+
+DATABASE = os.getenv("DATABASE", "news_db.json")
 NEWS_KEYWORD = os.getenv("NEWS_KEYWORD", "BPJS Ketenagakerjaan")
 NEWSAPI_KEY = os.getenv("NEWSAPI_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
