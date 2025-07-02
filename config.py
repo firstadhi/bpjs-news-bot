02be444870cd984016205b0cdc8e47e751394b78
firstadhi <44322688+firstadhi@users.noreply.github.com>
2025-07-02 15:11:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TELEGRAM_TOKEN = 'https://api.telegram.org/bot7595723761:AAE51oW9XFrvCpqcq83yx-q9XkCNKUKvKJA/setWebhook?url=https://bpjs-news-monitor-1.onrender.com/webhook'
+TELEGRAM_TOKEN = '7595723761:AAE51oW9XFrvCpqcq83yx-q9XkCNKUKvKJA'
 CHAT_ID = '846561600'
 DATABASE = 'news.db'
 NEWS_KEYWORD = 'BPJS Ketenagakerjaan'
