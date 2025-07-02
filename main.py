528fad7736b940bf4bb1d07470ae7084e7b574e7
firstadhi <44322688+firstadhi@users.noreply.github.com>
2025-07-02 17:37:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
 import requests
 import config
 
 def set_webhook():
-    url = f"https://api.telegram.org/bot{config.TELEGRAM_TOKEN}/setWebhook"
     webhook_url = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/{config.TELEGRAM_TOKEN}"
+    url = f"https://api.telegram.org/bot{config.TELEGRAM_TOKEN}/setWebhook"
     response = requests.post(url, json={"url": webhook_url})
     print("Webhook response:", response.text)
 
